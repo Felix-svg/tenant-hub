e8cb27d1074584b1053c13a89e19675dfcafdc25
Felix Omondi <felixomosh7@gmail.com>
2024-07-25 19:19:25 +0300

    Complete tenant routes

--- routes/tenants.py
+++ routes/tenants.py
@@ -2,14 +2,119 @@ from flask import make_response, jsonify, request
 from flask_restful import Resource
 from models.tenant import Tenant
 from config import db
-from utils import role_required
+from utils import role_required, not_found, no_input_data, server_error, missing_fields
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 class Tenants(Resource):
-    pass
+    @jwt_required()
+    @role_required('manager')
+    def get(self):
+        try:
+            tenants = [tenant.to_dict(rules=['-apartment', '-manager']) for tenant in Tenant.query.all()]
+            return make_response(jsonify({'tenants': tenants}))
+        except Exception as e:
+            return server_error(e)
+
+    @jwt_required()
+    @role_required('manager')
+    def post(self):
+        try:
+            data = request.get_json()
+            if not data:
+                return no_input_data()
+
+            name = data.get('name')
+            email = data.get('email')
+            phone_number = data.get('phone_number')
+            password = data.get('password')
+            apartment_id = data.get('apartment_id')
+            manager_id = data.get('manager_id')
+
+            if not name or not email or not phone_number or not apartment_id or not manager_id:
+                return missing_fields()
+
+            new_tenant = Tenant(name=name, email=email, phone_number=phone_number, apartment_id=apartment_id, manager_id=manager_id)
+            new_tenant.set_password(password)
+
+            db.session.add(new_tenant)
+            db.session.commit()
+
+            return make_response(jsonify({'message': 'Tenant create successfully'}), 201)
+        except Exception as e:
+            db.session.rollback()
+            return server_error(e)
 
 
 class TenantByID(Resource):
-    pass
+    @jwt_required()
+    @role_required('manager')
+    def get(self, id):
+        try:
+            tenant = Tenant.query.filter(Tenant.id == id).first()
+            if not tenant:
+                return not_found('Tenant')
+
+            tenant_dict = tenant.to_dict(rules=['-apartment', '-manager'])
+            return make_response(jsonify({'tenant': tenant_dict}), 200)
+        except Exception as e:
+            return server_error(e)
+
+    @role_required('manager')
+    def patch(self, id):
+        try:
+            tenant = Tenant.query.filter(Tenant.id == id).first()
+            if not tenant:
+                return not_found('Tenant')
+
+            data = request.get_json()
+            if not data:
+                return no_input_data()
+
+            name = data.get('name')
+            email = data.get('email')
+            phone_number = data.get('phone_number')
+            password = data.get('password')
+            apartment_id = data.get('apartment_id')
+            manager_id = data.get('manager_id')
+
+            if name is not None:
+                tenant.name = name
+
+            if email is not None:
+                tenant.email = email
+
+            if phone_number is not None:
+                tenant.phone_number = phone_number
+
+            if password is not None:
+                tenant.set_password(password)
+
+            if apartment_id is not None:
+                tenant.apartment_id = apartment_id
+
+            if manager_id is not None:
+                tenant.manager_id = manager_id
+
+            db.session.commit()
+            return make_response(jsonify({'message': 'Tenant updated successfully'}), 200)
+
+        except Exception as e:
+            db.session.rollback()
+            return server_error(e)
+
+    @jwt_required()
+    @role_required('manager')
+    def delete(self, id):
+        try:
+            tenant = Tenant.query.filter(Tenant.id == id).first()
+            if not tenant:
+                return not_found('Tenant')
+
+            db.session.delete(tenant)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Tenant deleted successfully'}), 200)
+        except Exception as e:
+            db.session.rollback()
+            return server_error(e)
 
